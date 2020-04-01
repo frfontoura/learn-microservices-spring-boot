85d091b34e573ecd9d456c92f9bb2bcf11bd2545
frfontoura <frfontoura@gmail.com>
2020-04-01 13:04:26 -0300

    Added cors configuration to multiplication microservice

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/WebConfiguration.java
@@ -0,0 +1,26 @@
+package microservices.book.multiplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author frfontoura
+ * @version 1.0 01/04/2020
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
